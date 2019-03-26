6618aa95bb1b5a7d6b0a9724b79e56a81a2e6bfa
TiuFraachz <war.perecin21@gmail.com>
2019-03-26 19:36:19 -0300

    bost

--- comandos/privado.js
+++ comandos/privado.js
@@ -14,29 +14,21 @@ module.exports.run = async (client, message, args) => {
       return message.channel.send("**Por favor, nao esqueca de inserir seu anuncio!**")
    }
 
-    let emj = ':Party:';
-    let emj2 = ':Drogadao:';
-    let emj3 = ':Glitch:';
+    let emj1 = '<:email:531064750000308224>';
+    let emj2 = '<:visto:531127230068424708>';
+    let emj3 = '<:canais:531535896281874487>';
     let canal = new Discord.RichEmbed()
 
     .setTitle(emj2 + " | **Anuncio Enviado!**")
-    .setDescription("\n" + emj3 + " | Anuncio enviado para Todos do grupo!\nNao Abuse de min!**" + emj1)
+    .setDescription("\n" + emj3 + " **| Anuncio enviado para Todos do grupo!\nNao Abuse de min! **" + emj1)
     .setColor("#07ed66")
     .setTimestamp(new(Date))
     .setThumbnail(message.author.avatarURL)
     .setFooter("Comando Requisitado por: " + nomeeapelido, message.author.avatarURL);
 
     message.channel.send(canal)
-
-    let privado = new Discord.RichEmbed()
-
-    .setDescription(`${sMsg}`)
-    .setColor("07ed66")
-    .setTimestamp(new(Date))
-    .setThumbnail(message.author.avatarURL)
-    .setFooter(`Fraachz_#4191`)
   
-    message.guild.members.forEach(user => user.send(privado).catch((erro) => { console.log("nao foi possivel enviar para todos usuarios, alguns estao com a dm bloqueada.")
+    message.guild.members.forEach(user => user.send(sMsg).catch((erro) => { console.log("nao foi possivel enviar para todos usuarios, alguns estao com a dm bloqueada.")
         })
       )
   }
