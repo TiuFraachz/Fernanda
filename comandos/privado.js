fc8cf72090fe221ae63b7aba1c25ae7234fb6c95
TiuFraachz <war.perecin21@gmail.com>
2019-03-29 21:50:40 -0300

    qualquer

--- comandos/privado.js
+++ comandos/privado.js
@@ -4,7 +4,7 @@ module.exports.run = async (client, message, args) => {
 
     message.delete();
 
-    if (message.author.id !== "446857017429196810") 
+    if (message.author.id !== "559864939716804648") 
     return message.reply("⚠️ **|** Você não pode executar este comando!");
     
     let sMsg = args.join(' ');
