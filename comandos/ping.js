0f966e794e2f4a06a9ee926f0cc961b5077d4a2a
TiuFraachz <war.perecin21@gmail.com>
2019-04-07 16:57:10 -0300

    a

--- comandos/ping.js
+++ comandos/ping.js
@@ -1,24 +1,25 @@
 const Discord = require("discord.js");
 
-module.exports.run = async (bot, message, args) => {
+module.exports.run = async (client, message, args) => {
     
     message.delete(); 
 
     let creator = 'Fraachz_#4191';
 
-    let ping = new Discord.RichEmbed()
+        let pign = new Discord.RichEmbed()
     
-    .setTitle('***Pong 🏓***')
-    .setColor('RANDOM')
-    .setThumbnail(message.guild.iconURL)
-    .setDescription(`**Meu ping?**\n\n
-    Checkado! Minha Latência é de **${Math.round(bot.ping)}ms.`)
-    .setImage("https://cdn.discordapp.com/attachments/529064545478508544/547041937073373194/rainbow.gif") 
-    .setFooter(`${creator} • © 2019`, client.user.displayAvatarURL)
+            .setTitle('***Pong 🏓***')
+            .setColor('RANDOM')
+            .setThumbnail(message.guild.iconURL)
+            .setDescription(`**Meu ping?**\n\n
+            Checkado! Minha Latência é de **${Math.round(client.ping)}ms.`)
+            .setImage("https://cdn.discordapp.com/attachments/529064545478508544/547041937073373194/rainbow.gif") 
+            .setFooter(`${creator} • © 2019`, client.user.displayAvatarURL)
+    
+            message.channel.send(pign)
 
-    message.channel.send(ping);
-    }
-
+        }
+        
 exports.help = {
 	name: "ping" 
 }
